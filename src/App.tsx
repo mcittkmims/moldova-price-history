f738b6ad253ea62fe8de38e32ffc6ad4820c6517
mcittkmims <adrian.vremere.swe@gmail.com>
2026-04-16 17:45:13 +0300

    add chart components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { AppLayout } from "./components/layout/AppLayout";
-
-function PlaceholderPage() {
-  return <div className="text-sm text-ink-700 dark:text-neutral-300" />;
-}
+import { HomePage } from "./pages/HomePage";
+import { ProductDetailsPage } from "./pages/ProductDetailsPage";
+import { SearchPage } from "./pages/SearchPage";
+import { TrackedProductsPage } from "./pages/TrackedProductsPage";
 
 export default function App() {
   return (
     <Routes>
       <Route element={<AppLayout />}>
         <Route index element={<Navigate to="/home" replace />} />
-        <Route path="/home" element={<PlaceholderPage />} />
-        <Route path="/search" element={<PlaceholderPage />} />
-        <Route path="/products/:productId" element={<PlaceholderPage />} />
-        <Route path="/tracked" element={<PlaceholderPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/products/:productId" element={<ProductDetailsPage />} />
+        <Route path="/tracked" element={<TrackedProductsPage />} />
       </Route>
     </Routes>
   );
