36bad741bdaac08fbc7721a53d327b7b16a4fe74
mcittkmims <adrian.vremere.swe@gmail.com>
2026-05-12 16:28:17 +0300

    fix auth page flash on load

---
+++ src/app/register/loading.tsx
@@ -0,0 +1,7 @@
+export default function RegisterLoading() {
+  return (
+    <div className="mx-auto flex min-h-[calc(100vh-12rem)] w-full max-w-[420px] items-center">
+      <div className="w-full rounded-lg border border-ink-200 bg-white p-6 dark:border-neutral-800 dark:bg-[#171717]" />
+    </div>
+  );
+}
